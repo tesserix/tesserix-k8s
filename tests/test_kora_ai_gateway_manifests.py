040c14bd34216c0bc566ef3a15d21a41940f6d26
Samyak Rout <samyak.rout@gmail.com>
2026-08-19 10:48:36 +1000

    fix: deploy CPU-only token optimizer image

--- tests/test_kora_ai_gateway_manifests.py
+++ tests/test_kora_ai_gateway_manifests.py
@@ -212,28 +212,25 @@ class KoraAIGatewayManifestTests(unittest.TestCase):
         gateway_egress = network_policy["spec"]["egress"][1]
         self.assertEqual(8080, gateway_egress["ports"][0]["port"])
 
-    def test_token_optimizer_pulls_large_private_image_without_remote_cache(self):
+    def test_token_optimizer_pulls_cpu_image_from_first_party_registry(self):
         documents = render_chart(
             "charts/apps/token-optimizer",
             "token-optimizer",
             "agentgateway-system",
         )
         deployment = resource(documents, "Deployment", "token-optimizer")
-        pull_secret = resource(documents, "ExternalSecret", "token-optimizer-ghcr")
         pod = deployment["spec"]["template"]["spec"]
+        image = pod["containers"][0]["image"]
 
         self.assertEqual(
-            "ghcr.io/tesserix/token-optimizer",
-            pod["containers"][0]["image"].split("@")[0],
+            "asia-south1-docker.pkg.dev/tesseracthub-480811/global/token-optimizer",
+            image.split("@")[0],
         )
         self.assertEqual(
-            [{"name": "token-optimizer-ghcr"}],
-            pod["imagePullSecrets"],
+            "sha256:58958f8dc92ab4ab70183c203ab92cfac26d0d716482adfc6f7ebbd6a9bad41a",
+            image.split("@")[1],
         )
-        self.assertEqual(
-            {"prod-ghcr-username", "prod-ghcr-token"},
-            {entry["remoteRef"]["key"] for entry in pull_secret["spec"]["data"]},
-        )
+        self.assertNotIn("imagePullSecrets", pod)
 
     def test_registry_publish_path_is_credential_gated(self):
         registry = render_chart(
