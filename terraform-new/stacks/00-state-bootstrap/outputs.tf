80397724305a3bc1ac5e61c672b7807d4930a364
sam123ben <samyak.rout@gmail.com>
2026-08-25 00:04:50 +1000

    test(atlantis): verify required apply status

--- terraform-new/stacks/00-state-bootstrap/outputs.tf
+++ terraform-new/stacks/00-state-bootstrap/outputs.tf
@@ -2,3 +2,8 @@ output "state_bucket_name" {
   description = "Terraform state bucket name."
   value       = google_storage_bucket.terraform_state.name
 }
+
+output "atlantis_apply_gate_probe" {
+  description = "Disposable output used to verify the required apply status."
+  value       = "not-applied"
+}
